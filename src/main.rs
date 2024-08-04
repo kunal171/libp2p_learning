81c2e3aa576352b5009161a07dfc9afc97c0889e
kunal171 <kunalkunal63@gmail.com>
2024-08-04 22:32:17 +0530

    idle connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use libp2p::ping;
 use std::error::Error;
+use std::time::Duration;
 use tracing_subscriber::EnvFilter;
 
 #[async_std::main]
@@ -14,6 +15,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             libp2p::yamux::Config::default,
         )?
         .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30))) // Allows us to observe pings for 30 seconds.
         .build();
 
     Ok(())
